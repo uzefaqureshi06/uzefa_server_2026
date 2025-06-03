f6310370a29609e82e5e319deceb4bfa2a1773a5
Uzefa Qureshi <uzefaqureshi06@gmail.com>
2025-06-03 20:32:00 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
  const connectToDB =async()=>{
     try {
-        await mongoose.connect('mongodb+srv://avez-taxlab:WMw2nFx5fDUQw0h4@cluster0.nwg85.mongodb.net/vat-db?retryWrites=true&w=majority&appName=Cluster0USE_MONGO_AS_CLUSTER = true')
+      await mongoose.connect('mongodb+srv://avez-taxlab:avez-taxlab@cluster0.nwg85.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
     console.log('Connected to DB')
     } catch (error) {
         console.log('Error connecting to DB',error)
